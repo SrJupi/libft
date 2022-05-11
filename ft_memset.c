f936f355224200b05957f52b74bdfae481f4a979
Lucas Sulzbach rilho <lsulzbac@cbr1s1.42barcelona.com>
2022-05-11 19:03:25 +0200

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,29 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/09 16:11:31 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/11 18:44:22 by lsulzbac         ###   ########.fr       */
+/*   Created: 2022/05/11 17:57:31 by lsulzbac          #+#    #+#             */
+/*   Updated: 2022/05/11 19:02:50 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
+#include <string.h>
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
 
-#endif
+	i = 0;
+	while (i < len)
+	{
+		*(unsigned char *)(b + i) = c;
+		i++;
+	}
+	return (b);
+}
