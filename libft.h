f5ef9a00c17a03bb3aaa059bb9633d4922511f65
Lucas Sulzbach rilho <lsulzbac@car12s1.42barcelona.com>
2022-05-13 17:11:23 +0200

    ft_strlcat.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:11:31 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/13 14:45:21 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:29:25 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,14 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len); //workhard
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t	ft_strlcat(char * dst, const char * src, size_t dstsize);
+
 //memcpy
 //memmove
 //strlcpy
-//strlcat
+//calloc
+//strdup
 //strjoin
 //strtrim
 //split
