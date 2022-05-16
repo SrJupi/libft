dc16bd0e3b63242e6c992dc040e31b54bb77bd5d
Lucas Sulzbach rilho <lsulzbac@cbr1s1.42barcelona.com>
2022-05-16 18:13:42 +0200

    ft_putnbr_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:11:31 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/16 14:59:11 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:33:16 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 //char	*ft_strtrim(char const *s1, char const *set);
 
@@ -51,9 +52,5 @@ void	ft_putendl_fd(char *s, int fd);
 //itoa
 //strmapi
 //striteri
-//putchar
-//putstr
-//putendl
-//putnbr
 
 #endif
