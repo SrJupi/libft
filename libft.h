3c6d1e7ac4cdecabc9f96006283fa3323664ed6c
Lucas Sulzbach rilho <lsulzbac@cbr1s1.42barcelona.com>
2022-05-16 14:59:54 +0200

    ft_putendl_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:11:31 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/16 14:53:23 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2022/05/16 14:59:11 by lsulzbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+
 //char	*ft_strtrim(char const *s1, char const *set);
 
 //split
